e5364b39a156ca0bcf657bdd2da462826bfcb70c
Aayush Saw <aayushsaw@example.com>
2026-01-06 15:06:19 +0530

    CRITICAL: Fixed fetch_cart.php 500 error - initialize cart array

--- fetch_cart.php
+++ fetch_cart.php
@@ -1,8 +1,12 @@
-<?php
+\u003c?php
 session_start();
+// Initialize cart if it doesn't exist
+if (!isset($_SESSION['cart'])) {
+    $_SESSION['cart'] = array();
+}
 $id = $_GET['id'];
 if (!in_array($id, $_SESSION['cart'])) {
-	$_SESSION['cart'][] = $id;
+\t$_SESSION['cart'][] = $id;
 }
 echo json_encode($_SESSION['cart']);
-?>
+?\u003e
